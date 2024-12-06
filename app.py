7e078c85a8766ec79e1aa1664c49ba3ef4cea982
Shashi Obulasetty <shashi.osp2@gmail.com>
2024-12-06 16:35:24 -0600

    version2

--- app.py
+++ app.py
@@ -13,3 +13,29 @@ def create_table():
     conn.close()
 
 create_table()
+
+@app.route('/api/items', methods=['GET'])
+def get_items():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM items")
+    items = cursor.fetchall()
+    conn.close()
+    return jsonify(items)
+
+@app.route('/api/items', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    name = data.get('name')
+    quantity = data.get('quantity')
+
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO items (name, quantity) VALUES (?, ?)", (name, quantity))
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Item added'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
